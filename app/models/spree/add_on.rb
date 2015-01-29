0929f725fb3a68876f15594edf519009ce5879ca
darbs <darby.perez@gmail.com>
2015-01-29 15:05:35 -0600

    images

--- app/models/spree/add_on.rb
+++ app/models/spree/add_on.rb
@@ -3,6 +3,7 @@ class Spree::AddOn < Spree::Base
 
   has_and_belongs_to_many :products
   has_many :adjustments, as: :source
+  has_many :images, -> { order(:position) }, as: :viewable, dependent: :destroy, class_name: "Spree::Image"
 
   validates :name, presence: true
   validates :sku, presence: true
--- spec/models/spree/add_on_spec.rb
+++ spec/models/spree/add_on_spec.rb
@@ -50,4 +50,23 @@ describe Spree::AddOn do
 
   end
 
+  describe "#images" do
+    let(:add_on) { create(:add_on) }
+    let(:image) { File.open(File.expand_path('../../../fixtures/nacho_taco.png', __FILE__)) }
+    let(:params) { {:viewable_id => add_on.id, :viewable_type => 'Spree::AddOn', :attachment => image, :alt => "position 2", :position => 2} }
+
+    before do
+      Spree::Image.create(params)
+      Spree::Image.create(params.merge({:alt => "position 1", :position => 1}))
+    end
+
+    it "only looks for variant images" do
+      expect(add_on.images.size).to eq(2)
+    end
+
+    it "should be sorted by position" do
+      expect(add_on.images.pluck(:alt)).to eq(["position 1", "position 2"])
+    end
+  end
+
 end
